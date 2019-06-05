8f7260183c1e9397809c664a5b8cd013a93dc2df
MIhzuron <49231083+MIhzuron@users.noreply.github.com>
2019-06-05 12:02:29 +0300

    Update change-password.php

--- change-password.php
+++ change-password.php
@@ -73,7 +73,7 @@ if(isset($_POST['newPassSubmit'])) //the user has entered a new pass- lets updat
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/logo2.jpg">
     <title>מיחזורון-איפוס ססמא</title>
     <link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600,700" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
